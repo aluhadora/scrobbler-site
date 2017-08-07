40be4294754b04fc693cd54d5cec5f2ce1fe0c82
Allan T. Harper <ath@caselle.com>
2017-08-07 17:00:09 -0600

    Error Display

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 import Track from './Track.js';
 import * as Actions from './Actions.js';
-
+var $ = require('jQuery');
 
 class App extends Component {
   updateTracks(context) {
     Actions.pullTracks(obj => {
+      console.log(obj);
       context.setState(function () {
         return {
-          tracks: obj.recenttracks.track,
+          tracks: obj.error ? obj: obj.recenttracks.track,
         };
       });
     }, context.state ? context.state.user : new URL(window.location).searchParams.get('user'));
@@ -19,6 +20,7 @@ class App extends Component {
 
     var url = new URL(window.location);
     var user = url.searchParams.get('user');
+    user = user ? user : Actions.user();
     if (user) this.setState(function () {
       return {
         user: user
@@ -28,6 +30,19 @@ class App extends Component {
     var context = this;
     this.updateTracks(context);
     setInterval(() => this.updateTracks(context), 5000);
+
+
+  }
+
+  componentDidUpdate() {
+    document.querySelector('#lastFm').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    if (key === 13) {
+      if ($(this).val()) {
+        window.location.href = "/?user=" + $(this).val();
+      }
+    }
+    });
   }
 
   render() {
@@ -44,11 +59,22 @@ class App extends Component {
         date={track.date} index={i} />);
     }
 
+    var message = (<div></div>);
+    if (tracks.error) message = (
+      <p className="App-intro">
+        {tracks.message}
+      </p>
+    );
+
     return (
       <div className="App">
+        <p className="App-intro" style={{ paddingLeft: '1em', paddingBottom: '1em'}}>
+          User: <input type="text" id="lastFm" defaultValue={user}></input>
+        </p>
         <p className="App-intro">
           Pulling recent tracks from <code>{user}</code>.
         </p>
+        {message}
         {trackControls}
       </div>
     );
