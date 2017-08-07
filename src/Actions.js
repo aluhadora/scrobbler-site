06f8eb849fa28cf51fc5c64452b1f8e27eb765fb
Allan T. Harper <ath@caselle.com>
2017-08-07 16:21:35 -0600

    Refreshing page

--- src/Actions.js
+++ src/Actions.js
@@ -3,15 +3,21 @@ var $ = require('jQuery');
 var api = '';
 var defaultUser = 'aluhadora';
 
-export function pullTracks (callback) {
+export function pullTracks (callback, user) {
+  if (!user) user = defaultUser;
+
   var url = 'https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks' +
-   '&user=' + defaultUser + '&api_key=' + api + '&format=json&limit=5';
+   '&user=' + user + '&api_key=' + api + '&format=json&limit=5';
 
    $.ajax({
      method: 'Get',
      url: url,
-     async: false,
+     async: false
    }).done(data => {
      callback(data);
    });
+}
+
+export function user () {
+  return defaultUser;
 }
--- src/App.js
+++ src/App.js
@@ -5,27 +5,51 @@ import * as Actions from './Actions.js';
 
 
 class App extends Component {
-  render() {
-    var tracks = [];
-
+  updateTracks(context) {
     Actions.pullTracks(obj => {
-      var trackInfos = obj.recenttracks.track;
+      context.setState(function () {
+        return {
+          tracks: obj.recenttracks.track,
+        };
+      });
+    }, context.state ? context.state.user : new URL(window.location).searchParams.get('user'));
+  }
 
-      for (var i = 0; i < trackInfos.length; i++) {
-        var track = trackInfos[i];
-        console.log(track);
-        tracks.push(<Track key={i} name={track.name} artist={track.artist['#text']}
-          album={track.album['#text']} img={track.image[2]['#text']} attr={track.attr}
-          date={track.date} index={i} />);
-      }
+  componentDidMount() {
+
+    var url = new URL(window.location);
+    var user = url.searchParams.get('user');
+    if (user) this.setState(function () {
+      return {
+        user: user
+      };
     });
+
+    var context = this;
+    this.updateTracks(context);
+    setInterval(() => this.updateTracks(context), 5000);
+  }
+
+  render() {
+    if (!this.state) return (<div></div>);
+
+    console.log(this.props);
+    const { tracks, user } = this.state;
+    var trackControls = [];
+
+    for (var i = 0; i < tracks.length; i++) {
+      var track = tracks[i];
+      trackControls.push(<Track key={i} name={track.name} artist={track.artist['#text']}
+        album={track.album['#text']} img={track.image[2]['#text']} attr={track.attr}
+        date={track.date} index={i} />);
+    }
 
     return (
       <div className="App">
         <p className="App-intro">
-          Pulling recent tracks from <code>aluhadora</code>.
+          Pulling recent tracks from <code>{user}</code>.
         </p>
-        {tracks}
+        {trackControls}
       </div>
     );
   }
