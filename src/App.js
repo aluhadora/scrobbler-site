d118cd6d22f752b9755159a6db56c10d9c60b4ac
Allan T. Harper <ath@caselle.com>
2017-08-07 13:22:22 -0600

    Pulling from LastFM api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Track from './Track.js';
+import * as Actions from './Actions.js';
+
 
 class App extends Component {
   render() {
+    var tracks = [];
+
+    Actions.pullTracks(obj => {
+      var trackInfos = obj.recenttracks.track;
+
+      for (var i = 0; i < trackInfos.length; i++) {
+        var track = trackInfos[i];
+        console.log(track);
+        tracks.push(<Track key={i} name={track.name} artist={track.artist['#text']}
+          album={track.album['#text']} img={track.image[2]['#text']} attr={track.attr}
+          date={track.date} index={i} />);
+      }
+    });
+
     return (
       <div className="App">
         <p className="App-intro">
           Pulling recent tracks from <code>aluhadora</code>.
         </p>
-        <Track />
+        {tracks}
       </div>
     );
   }
--- src/Track.js
+++ src/Track.js
@@ -1,26 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class Track extends Component {
+
+  timeline(timeDiff, timeName) {
+    var truncedTimeDiff = Math.trunc(timeDiff);
+    return truncedTimeDiff + ' ' + timeName + (truncedTimeDiff === 1 ? '' : 's') + ' ago';
+  }
+
+  dateline(attr, date) {
+    if (attr && attr.nowplaying) return 'Now playing';
+    if (!date) return 'Now playing';
+    // return date.uts;
+    var d = new Date(0);
+    d.setUTCSeconds(date.uts);
+    var timeDiff = Date.now() - date.uts * 1000;
+
+    var secondsDiff = timeDiff / 1000;
+    if (secondsDiff < 60) return this.timeline(secondsDiff, 'second');
+
+    var minutesDiff = secondsDiff / 60;
+    if (minutesDiff < 60) return this.timeline(minutesDiff, 'minute');
+
+    var hoursDiff = minutesDiff / 60;
+    if (hoursDiff < 24) return this.timeline(hoursDiff, 'hour');
+
+    var daysDiff = hoursDiff / 60;
+    return this.timeline(daysDiff, 'day');
+  }
+
   render() {
+    const { name, artist, album, img, attr, date, index } = this.props;
+
     return (
-      <div className="Track">
+      <div className="Track" style={{opacity: index === 0 ? '1' : '0.5'}}>
         <div className="Album-Art">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={img} alt="logo" height="96" />
         </div>
         <div className="Track-Info">
           <div className="Info-line">
-            <span className="Info-Field">Track:</span> A song
+            <span className="Info-Field">Track:</span> {name}
           </div>
           <div className="Info-line">
-            <span className="Info-Field">Artist:</span> An artist
+            <span className="Info-Field">Artist:</span> {artist}
           </div>
           <div className="Info-line">
-            <span className="Info-Field">Album:</span> An album
+            <span className="Info-Field">Album:</span> {album}
           </div>
-          <div className="Info-line">
-            <span className="Info-Field">Now playing</span>
+          <div className="Info-line" style={{paddingTop: '6px'}}>
+            <span className="Info-Field">{this.dateline(attr, date)}</span>
           </div>
         </div>
       </div>
